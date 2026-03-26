81d89d74c62cd46b875cec128b8b648b5b82e2ad
Warnakula S P <it24101208@my.sliit.lk>
2026-03-26 12:02:29 +0530

    Basic Structure Frontend Completion

--- project/src/main/java/com/vendora/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.vendora.service;
-
-import com.vendora.dto.RegisterRequest;
-import com.vendora.model.User;
-
-public interface UserService {
-    User register(RegisterRequest request);
-}

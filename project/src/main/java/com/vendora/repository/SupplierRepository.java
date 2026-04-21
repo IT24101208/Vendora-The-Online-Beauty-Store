64fb05c18f25c17f4d84ffec654d22cd365acc4f
IT24102671 <it24102671@my.sliit.lk>
2026-04-21 14:22:00 +0530

    Delete project/src/main/java/com/vendora/repository/SupplierRepository.java

--- project/src/main/java/com/vendora/repository/SupplierRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vendora.repository;
-
-import com.vendora.model.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepository extends JpaRepository<Supplier, Long> {
-}

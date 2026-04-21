ead92d0aff8e18db144948b0af7f7ba539c34a1e
Nethmi Thakshila <it24100755@my.sliit.lk>
2026-04-21 17:49:52 +0530

    Create OrderRepository.java

---
+++ project/src/main/java/com/vendora/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.vendora.repository;
+
+import com.vendora.cart.model.Order;
+import com.vendora.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserOrderByCreatedAtDesc(User user);
+}
